ada122ac4c7b23f3645d99c22e89e922dbb768b7
abigail.ojeda <abigail.ojeda@bitboxonline.com>
2022-07-13 11:26:39 +0100

    api fecth fix

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,5 @@
 export const getGifs = async(category) =>{
-    const url= `http://api.giphy.com/v1/gifs/search?api_key=wQ78An7Jz2oamTkf9hCcp93lGyed8hSW&limit=10&q=${category}`;
+    const url= `https://api.giphy.com/v1/gifs/search?api_key=wQ78An7Jz2oamTkf9hCcp93lGyed8hSW&limit=10&q=${category}`;
     const resp = await fetch(url);
     const {data} = await resp.json();
 
